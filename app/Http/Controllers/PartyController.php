daf75e111a2357ab7751f458b8d61030950f11ff
Katsura34 <157209871+Katsura34@users.noreply.github.com>
2025-10-23 20:48:09 +0800

    Add PartyController for create/update/delete under election

---
+++ app/Http/Controllers/PartyController.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Election;
+use App\Models\Party;
+use Illuminate\Http\Request;
+use Illuminate\Support\Str;
+
+class PartyController extends Controller
+{
+    public function store(Request $request, Election $election)
+    {
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'color' => 'nullable|string|max:20',
+            'description' => 'nullable|string',
+        ]);
+
+        $data['slug'] = Str::slug($data['name']);
+        $data['election_id'] = $election->id;
+
+        Party::create($data);
+
+        return back()->with('success', 'Party added.');
+    }
+
+    public function update(Request $request, Election $election, Party $party)
+    {
+        $this->authorizeParty($election, $party);
+
+        $data = $request->validate([
+            'name' => 'required|string|max:255',
+            'color' => 'nullable|string|max:20',
+            'description' => 'nullable|string',
+        ]);
+        $data['slug'] = Str::slug($data['name']);
+
+        $party->update($data);
+
+        return back()->with('success', 'Party updated.');
+    }
+
+    public function destroy(Election $election, Party $party)
+    {
+        $this->authorizeParty($election, $party);
+
+        $party->delete();
+        return back()->with('success', 'Party deleted.');
+    }
+
+    protected function authorizeParty(Election $election, Party $party): void
+    {
+        abort_unless($party->election_id === $election->id, 404);
+    }
+}
